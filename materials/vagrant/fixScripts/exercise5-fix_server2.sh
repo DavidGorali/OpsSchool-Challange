dda1d72a8d2fdc6d4ec045f2a2823af6be75d43c
David Gorali <davidstar1998@gmail.com>
2023-08-19 22:09:47 +0300

    solution

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -2,9 +2,11 @@
 #add fix to exercise5-server2 here
 #Server2 Fix
 sudo sed -i '$a\    StrictHostKeyChecking no' /etc/ssh/ssh_config
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
 runuser -l vagrant -c "ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa"
 sudo apt update && sudo apt install sshpass -y
-sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa vagrant@server1
+sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa vagrant@192.168.60.10
 #Server1 Fix 
-runuser -l vagrant -c "ssh vagrant@server1 'sudo apt install sshpass -y'"
-runuser -l vagrant -c "ssh vagrant@server1 'sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa vagrant@server2'"
+runuser -l vagrant -c "ssh vagrant@192.168.60.10 'sudo apt install sshpass -y'"
+runuser -l vagrant -c "ssh vagrant@192.168.60.10 'sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa vagrant@192.168.60.11'"
